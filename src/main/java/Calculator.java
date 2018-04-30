a5c9d4d7e2c61cf075dfb93cce893e90f59d8563
Hadsan Geele <hgeele22@gmail.com>
2018-04-30 23:33:51 +0100

    Added , subtract, divide, multiply, addition methods

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -1,2 +1,29 @@
 public class Calculator {
+
+    private int number1;
+    private int number2;
+
+    public Calculator(int number1, int number2){
+
+        this.number1= number1;
+        this.number2 = number2;
+    }
+
+    public int getAdd(){
+        return this.number1+this.number2;
+    }
+
+    public int getMultiply(){
+        return this.number1*number2;
+    }
+
+    public int getDivide(){
+        return this.number1/number2;
+    }
+
+    public int getSubtract(){
+        return this.number1-this.number2;
+    }
+
+
 }
